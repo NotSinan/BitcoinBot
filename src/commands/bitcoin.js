2d153151b55d40575689c9922075f03e5bcc0c0a
Sinan <haxsmith77@gmail.com>
2022-12-07 16:30:38 +1100

    Finalised bitcoin function

--- src/commands/bitcoin.js
+++ src/commands/bitcoin.js
@@ -1,14 +1,9 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const QuickChart = require('quickchart-js');
+const wait = require('node:timers/promises').setTimeout;
 
-module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('bitcoin')
-    .setDescription('Displays price of Bitcoin.'),
-
-    async execute(interaction) {
-        await interaction.deferReply()
-        const json = await fetch('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1')
+const createChartUrl = async (url) => {
+        const json = await fetch(url)
         .then((response) => response.json())
 
         const chart = new QuickChart();
@@ -45,11 +40,62 @@ module.exports = {
         
         // Get the chart image URL
         const chartUrl = await chart.getShortUrl();
-        
-        // Print the chart URL
-        console.log(chartUrl);
+        return chartUrl;
+}
 
-         await interaction.editReply(chartUrl)
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('bitcoin')
+    .setDescription('Displays price of Bitcoin.'),
 
+    async execute(interaction) {
+        await interaction.deferReply()
+        const url = await createChartUrl('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1')
+
+        const row = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId('5m')
+            .setLabel('5m')
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setCustomId('30m')
+            .setLabel('30m')
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setCustomId('1h')
+            .setLabel('1h')
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setCustomId('1d')
+            .setLabel('1d')
+            .setStyle(ButtonStyle.Primary),
+        );
+
+        await interaction.editReply({content:url, components: [row]});
+
+         const collector = interaction.channel.createMessageComponentCollector({ time: 15000 });
+
+         collector.on('collect', async i => {
+          let chartUrl;
+          if (i.customId === '5m') {
+              chartUrl = await createChartUrl('https://api.coincap.io/v2/assets/bitcoin/history?interval=m5')
+              await i.update({ content: chartUrl, components: [row] });
+          } else if (i.customId === '30m') {
+              chartUrl = await createChartUrl('https://api.coincap.io/v2/assets/bitcoin/history?interval=m30')
+              await i.update({ content: chartUrl, components: [row] });
+          } else if (i.customId === '1h') {
+              chartUrl = await createChartUrl('https://api.coincap.io/v2/assets/bitcoin/history?interval=h1')
+              await i.update({ content: chartUrl, components: [row] });
+          } else if (i.customId === '1d') {
+              chartUrl = await createChartUrl('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1')
+              await i.update({ content: chartUrl, components: [row] });
+          }
+      });
+
+      collector.on('end', () => {
+        interaction.channel.send('Sorry, the time has run out for this interaction. Please try again if you would like to use this feature.')
+      });
+            
     }
 }
